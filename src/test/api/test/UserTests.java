cb9628846dbdfc1a5cfe57a2db57ab1a4b6fd8ed
Ved86055 <vedvshinde11@gmail.com>
2026-02-25 16:22:30 +0530

    Generating Logs

--- src/test/api/test/UserTests.java
+++ src/test/api/test/UserTests.java
@@ -9,6 +9,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -16,6 +19,7 @@ public class UserTests
 {
     Faker fr;
     User payload;
+    public Logger logger;
 
     @BeforeClass
     public void setupData()
@@ -31,16 +35,22 @@ public class UserTests
         payload.setPhone(fr.phoneNumber().cellPhone());
         payload.setLastName(fr.name().lastName());
         payload.setUsername(fr.name().username());
+
+        //Logs
+        logger= LogManager.getLogger(this.getClass());
     }
 
     @Test(priority = 1)
     public void testPostRequest()
     {
+        logger.info("-----------Creating user-----------------");
        Response response=UserEndPoints.createUser(payload);
         response.then().log().all();
 
         Assert.assertEquals(response.statusCode(),200);
        response.then().header("Content-Type",equalTo("application/json"));
+
+       logger.info("---------------------user created--------------------");
 
 
     }
@@ -49,6 +59,8 @@ public class UserTests
     @Test(priority = 2,dependsOnMethods = "testPostRequest")
     public void testGetUserByName()
     {
+        logger.info("-------------------Read user info----------------------- ");
+
         //username as para we have to pass from payload , so we have to fetch only username
       Response response =UserEndPoints.readUser(this.payload.getUsername());
         System.out.println(payload.getUsername());
@@ -56,11 +68,14 @@ public class UserTests
 
       response.then().body("username",equalTo(payload.getUsername()));
 
+      logger.info("----------------read user info------------------------------");
+
     }
 
         @Test(priority = 3)
     void testUpdateUserByName()
     {
+        logger.info("---------------------------updateing user info--------------------------");
         payload.setEmail(fr.internet().safeEmailAddress());
         payload.setFirstName(fr.name().firstName());
         payload.setLastName(fr.name().lastName());
@@ -73,11 +88,14 @@ public class UserTests
 
         Response responseafterupdate =UserEndPoints.readUser(this.payload.getUsername());
         Assert.assertEquals(responseafterupdate.statusCode(),200);
+
+        logger.info("--------------------------updated user info-------------------------------");
     }
 
     @Test(priority = 4)
     void testDeleteUserByName()
     {
+        logger.info("***************************************delete user*********************");
 
         Response response= UserEndPoints.deleteUser(this.payload.getUsername());
         response.then().log().all();
@@ -91,9 +109,12 @@ public class UserTests
     @Test(priority = 5)
     void verifyDeletion()
     {
+
+        logger.info("*******************verifying user deleted or not********************");
         Response responseafterdeletion=UserEndPoints.readUser(payload.getUsername());
 
         responseafterdeletion.then().statusCode(404);
+        logger.info("***********************user deleted successsfully*****************************");
 
 
     }
